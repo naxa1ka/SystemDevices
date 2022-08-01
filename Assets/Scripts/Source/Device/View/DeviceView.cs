2f619914fa641cbc98e4ae7ec4a7276a07bb2caf
ilya kuznetsov <i.kuznetsov@sbergames.ru>
2022-08-01 22:37:47 +0300

    mini clean up

--- Assets/Scripts/Source/Device/View/DeviceView.cs
+++ Assets/Scripts/Source/Device/View/DeviceView.cs
@@ -5,7 +5,6 @@ namespace Source
     public class DeviceView : MonoBehaviour, IDeviceView
     {
         [SerializeField] private PositionView _positionView;
-        
         public IView<UnityEngine.Vector3> Position => _positionView;
     }
 }
